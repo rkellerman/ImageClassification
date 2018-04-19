b640474b633675536f4593c67e6c46d88f6a5f6a
elizabeth <elizabeth.caronia@rutgers.edu>
2018-04-19 09:54:08 -0400

    mira

--- mira.py
+++ mira.py
@@ -55,7 +55,42 @@ class MiraClassifier:
     representing a vector of values.
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    bestWeights = {}
+    bestAccuracy = None
+    for c in Cgrid:
+      weights = self.weights.copy()
+      for n in range(self.max_iterations):
+        for i, datum in enumerate(trainingData):
+          bestScore = None 
+          bestY = None
+
+          for y in self.legalLabels:
+            score = datum * weights[y]
+            if score > bestScore or bestScore is None:
+              bestScore = score
+              bestY = y
+            actualY = trainingLabels[i]
+              
+            if bestY != actualY:
+              f = datum.copy()
+              tau = min(c, ((weights[bestY] - weights[actualY])* f + 1.0)/(2.0 *(f*f)))
+              print 'tau ', tau
+              f.divideAll(tau/1.0)
+
+              weights[actualY] = weights[actualY] + f
+              weights[bestY] = weights[bestY] - f
+      correct = 0
+      guesses = self.classify(validationData)
+      for i, guess in enumerate(guesses):
+        correct += (validationLabels[i] == guess and 1.0 or 0.0)
+      accuracy = correct / len(guesses)
+
+      if accuracy > bestAccuracy or bestAccuracy is None:
+        bestAccuracy = accuracy
+        bestWeights = weights
+
+    self.weights = bestWeights
+    #util.raiseNotDefined()
 
   def classify(self, data ):
     """
